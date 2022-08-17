8ffed3fc35dfa1c3e747d204071e5bd3fdccae9e
Theo Goulart <theo_goulart@hotmail.com>
2022-08-17 15:53:27 -0300

    add data fetching

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './Components/Card';
 import Select from 'react-select'; 
 
@@ -8,7 +8,7 @@ const houseOptions = [
   { value: 'Gryffindor', label: 'Gryffindor' },
   { value: 'Slytherin', label: 'Slytherin' },
   { value: 'Ravenclaw', label: 'Ravenclaw' },
-  { value: 'Hufflepuf', label: 'Hufflepuf' }
+  { value: 'Hufflepuff', label: 'Hufflepuff' }
 ];
 
 const ancestryOptions = [
@@ -19,118 +19,20 @@ const ancestryOptions = [
 ];
 
 function App() {
+  const [characters, setChatacters] = useState([]);
   const [houseFilter, setHouseFilter] = useState("");
   const [ancestryFilter, setAncestryFilter] = useState("");
 
   const filterHouse = (option) => setHouseFilter(option.value);
   const filterAncestry = (option) => setAncestryFilter(option.value);
 
-  var characters = [
-    {
-    "name": "Harry Potter",
-    "alternate_names": [],
-    "species": "human",
-    "gender": "male",
-    "house": "Gryffindor",
-    "dateOfBirth": "31-07-1980",
-    "yearOfBirth": 1980,
-    "wizard": true,
-    "ancestry": "half-blood",
-    "eyeColour": "green",
-    "hairColour": "black",
-    "wand": {
-    "wood": "holly",
-    "core": "phoenix feather",
-    "length": 11
-    },
-    "patronus": "stag",
-    "hogwartsStudent": true,
-    "hogwartsStaff": false,
-    "actor": "Daniel Radcliffe",
-    "alternate_actors": [],
-    "alive": true,
-    "image": "http://hp-api.herokuapp.com/images/harry.jpg"
-    },
-    {
-    "name": "Hermione Granger",
-    "alternate_names": [],
-    "species": "human",
-    "gender": "female",
-    "house": "Gryffindor",
-    "dateOfBirth": "19-09-1979",
-    "yearOfBirth": 1979,
-    "wizard": true,
-    "ancestry": "muggleborn",
-    "eyeColour": "brown",
-    "hairColour": "brown",
-    "wand": {
-    "wood": "vine",
-    "core": "dragon heartstring",
-    "length": ""
-    },
-    "patronus": "otter",
-    "hogwartsStudent": true,
-    "hogwartsStaff": false,
-    "actor": "Emma Watson",
-    "alternate_actors": [],
-    "alive": true,
-    "image": "http://hp-api.herokuapp.com/images/hermione.jpeg"
-    },
-    {
-    "name": "Ron Weasley",
-    "alternate_names": [
-    "Dragomir Despard"
-    ],
-    "species": "human",
-    "gender": "male",
-    "house": "Gryffindor",
-    "dateOfBirth": "01-03-1980",
-    "yearOfBirth": 1980,
-    "wizard": true,
-    "ancestry": "pure-blood",
-    "eyeColour": "blue",
-    "hairColour": "red",
-    "wand": {
-    "wood": "willow",
-    "core": "unicorn tail-hair",
-    "length": 14
-    },
-    "patronus": "Jack Russell terrier",
-    "hogwartsStudent": true,
-    "hogwartsStaff": false,
-    "actor": "Rupert Grint",
-    "alternate_actors": [],
-    "alive": true,
-    "image": "http://hp-api.herokuapp.com/images/ron.jpg"
-    },
-    {
-    "name": "Draco Malfoy",
-    "alternate_names": [],
-    "species": "human",
-    "gender": "male",
-    "house": "Slytherin",
-    "dateOfBirth": "05-06-1980",
-    "yearOfBirth": 1980,
-    "wizard": true,
-    "ancestry": "pure-blood",
-    "eyeColour": "grey",
-    "hairColour": "blonde",
-    "wand": {
-    "wood": "hawthorn",
-    "core": "unicorn tail-hair",
-    "length": 10
-    },
-    "patronus": "",
-    "hogwartsStudent": true,
-    "hogwartsStaff": false,
-    "actor": "Tom Felton",
-    "alternate_actors": [],
-    "alive": true,
-    "image": "http://hp-api.herokuapp.com/images/draco.jpg"
-    }
-  ];
+  useEffect(() => {
+    fetch('http://hp-api.herokuapp.com/api/characters')
+      .then(response => response.json())
+      .then(data => setChatacters(data))
+  }, []);
 
-  characters = characters.filter(v => {
+  const filteredCharacters = characters.filter(v => {
     return (!houseFilter || v.house == houseFilter) &&
       (!ancestryFilter || v.ancestry == ancestryFilter);
   });
@@ -148,7 +50,7 @@ function App() {
         </div>
       </header>
       <section className="Char-list">
-        {characters.map((c, i) => (<Card key={i} character={c}/>))}
+        {filteredCharacters.map((c, i) => (<Card key={i} character={c}/>))}
       </section>
     </div>
   );
